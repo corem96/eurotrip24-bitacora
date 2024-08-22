8d48a5360aa42b2316d04a20f10eb0df8d0a6f11
jorgescamilla <jorge.mendoza@telcel.com>
2024-08-21 18:32:04 -0600

    quita temporalmente imagen de tarjeta

--- src/components/PointerItem.vue
+++ src/components/PointerItem.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-const props = defineProps<{img: string}>()
+// const props = defineProps<{img: string}>()
 </script>
 
 <template>
 <div class="card shadow-sm specific-w-300 w-sm-100 mw-75 h-100 mx-auto m-5">
-  <img :src="`../src/assets/img/${ props.img }`" class="card-img-top" :alt="`${props.img}`">
+  <!-- <img :src="`../src/assets/img/${ props.img }`" class="card-img-top" :alt="`${props.img}`"> -->
   <div class="card-body">
     <div class="card-title d-md-flex">
       <slot name="heading"></slot>
